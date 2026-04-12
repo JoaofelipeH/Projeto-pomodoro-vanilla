0f813dec9895275862a7c3aee4921a73c27874f6
jaum <jaumfelipered1802@gmail.com>
2026-04-12 11:53:27 -0300

    primeiro commit, para formatar o pc

---
+++ script.js
@@ -0,0 +1,28 @@
+let tempo = 1500;
+let minutos;
+let segundo;
+let cronometro = document.getElementById("cronometro");
+let intervaloid;
+let padminutos = String(minutos).padStart(2, "0");
+let padsegundos = String(segundo).padStart(2, "0");
+//
+document.getElementById("buttom-start").addEventListener("click", function () {
+    //
+    clearInterval(intervaloid);
+    //
+    intervaloid = setInterval(function () {
+        //
+        tempo = tempo - 1;
+        minutos = Math.floor(tempo / 60);
+        segundo = tempo % 60;
+        padminutos = String(minutos).padStart(2, "0");
+        padsegundos = String(segundo).padStart(2, "0");
+        cronometro.textContent = `${padminutos}:${padsegundos}`;
+        //
+    }, 1000);
+    //  
+},);
+//
+document.getElementById("buttom-pause").addEventListener("click", function () {
+    clearInterval(intervaloid);
+});
